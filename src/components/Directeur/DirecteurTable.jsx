275c9a6995f3e9a2a1fc9c6fe27894122cff3d39
AitSyad <aitsyad.fatimazahra@outlook.com>
2024-07-18 11:45:48 +0100

    documentstatus

--- src/components/Directeur/DirecteurTable.jsx
+++ src/components/Directeur/DirecteurTable.jsx
@@ -16,7 +16,7 @@ function DirecteurTable() {
         const response = await axios.get('https://localhost:7153/Requests/list');
         const sortedData = response.data.sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate));
         setRequests(sortedData);
-        setFilteredRequests(sortedData); // Initialize filtered requests with all requests
+        // setFilteredRequests(sortedData); // Initialize filtered requests with all requests
       } catch (error) {
         console.error('Erreur lors de l\'envoi de la demande:', error);
       }
@@ -64,7 +64,7 @@ function DirecteurTable() {
 
           <Filtrage requests={requests} onFilteredRequests={setFilteredRequests} />
             
-        ((((((((  <div className="overflow-x-auto mt-10 w-full">
+        <div className="overflow-x-auto mt-10 w-full">
             <table className="bg-white border border-gray-200 w-full">
               <thead>
                 <tr className="bg-gray-100">
@@ -73,7 +73,8 @@ function DirecteurTable() {
                   <th className="text-left py-3 px-1 font-semibold text-sm">Expéditeur</th>
                   <th className="text-left py-3 px-1 font-semibold text-sm">Date d'envoie</th>
 
-                  <th className="text-left py-3 pl-16 font-semibold text-sm">Refferens</th>
+                  {/* <th className="text-left py-3 pl-16 font-semibold text-sm">Refferens</th> */}
+                  <th className="text-left py-3 pl-16 font-semibold text-sm">état</th>
 
                   <th className="text-left py-3 px-1 font-semibold text-sm"></th>
                 </tr>
@@ -87,7 +88,9 @@ function DirecteurTable() {
                     <td className="border-t py-3 px-4">{request.documentType}</td>
                     <td className="border-t py-3 px-4">{request.role}</td>
                     <td className="border-t py-3 px-4">{new Date(request.createdDate).toLocaleDateString()}</td>
-                    <td className="border-t py-3 px-4">{request.id}</td>
+                    {/* <td className="border-t py-3 px-4">{request.id}</td> */}
+                    
+                    <td className="border-t py-3 px-4">{request.documentStatus}</td>
                     <td className="border-t py-3 px-1 flex gap-6">
                       <button
                         className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
@@ -112,7 +115,7 @@ function DirecteurTable() {
               </tbody>
             </table>
             <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
-          </div>))))))))
+          </div>
 
         </div>
       </div>
